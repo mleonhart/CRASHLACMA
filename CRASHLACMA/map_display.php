a45da94402e4b0a579e0e60b274caa9fe3908795
at0mbxmb <at0mbxmb@gmail.com>
2014-06-08 14:51:50 -0700

    testing page autorefresh

--- CRASHLACMA/map_display.php
+++ CRASHLACMA/map_display.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta http-equiv="Refresh" content="5">
 
     <style type="text/css">
       html { height: 100% }
