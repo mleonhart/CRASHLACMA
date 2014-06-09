5bca8828aee0cb36915dcd4be237794be3ba237d
at0mbxmb <at0mbxmb@gmail.com>
2014-06-08 17:56:15 -0700

    twitter client now loops through all files available in the directory

--- CRASHLACMA/twitter_client.py
+++ CRASHLACMA/twitter_client.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 
+import os
+import shutil
+
 from twitter_json_parser import TwitterJsonParser 
 
 print("Running twitter client...")
 
-input_data = '../data_raw_tweets/sample_mixed_data.json'
-parser = TwitterJsonParser()
-print(parser.get_data_from_tweets(input_data))
+for root, _, files in os.walk('../data_raw_tweets/'):
+    for f in files:
+        fullpath = os.path.join(root, f)
+        print fullpath
+        parser = TwitterJsonParser()
+        print(parser.get_data_from_tweets(fullpath))
+        shutil.move(fullpath, '../data_processed_tweets/')
+
+		
