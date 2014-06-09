897f2e93533fe1406474e4bed6d8e6e669eb84ad
at0mbxmb <at0mbxmb@gmail.com>
2014-06-08 17:55:40 -0700

    fixed streamlistener to properly handle file switching

--- CRASHLACMA/slistener.py
+++ CRASHLACMA/slistener.py
@@ -5,12 +5,9 @@ class SListener(StreamListener):
 
     def __init__(self, api = None, fprefix = 'streamer'):
         self.api = api or API()
-        self.counter = 0
+        self.filecounter = 1
         self.fprefix = fprefix
-        self.output  = open(fprefix + '.' 
-                            + time.strftime('%Y%m%d-%H%M%S') + '.json', 'wb')
-        self.delout  = open('delete.txt', 'a')
-
+        
     def on_data(self, data):
 
         if  'in_reply_to_status' in data:
@@ -28,30 +25,15 @@ class SListener(StreamListener):
             return false
 
     def on_status(self, status):
+        
+
+        filename = self.fprefix + '.' + time.strftime('%Y%m%d-%H%M') + '_' + str(self.filecounter) + '.json'
+
+        self.output = open(filename, 'wb')
+        print('Writing to file %s') % filename
         self.output.write(status + "\n")
-        self.counter += 1
-		
-        if self.counter >= 20000:
-            self.output.close()
-            self.output = open('../streaming_data/' + self.fprefix + '.' 
-                               + time.strftime('%Y%m%d-%H%M%S') + '.json', 'wb')
-            self.counter = 0
+        self.filecounter += 1
+        self.output.close()
 
         return
 
-    def on_delete(self, status_id, user_id):
-        self.delout.write( str(status_id) + "\n")
-        return
-
-    def on_limit(self, track):
-        sys.stderr.write(track + "\n")
-        return
-
-    def on_error(self, status_code):
-        sys.stderr.write('Error: ' + str(status_code) + "\n")
-        return False
-
-    def on_timeout(self):
-        sys.stderr.write("Timeout, sleeping for 60 seconds...\n")
-        time.sleep(60)
-        return 
