3d65229831d521ab43d52b9a8af285e926cd89a8
carlynorama <carlynorama@gmail.com>
2014-07-11 14:58:23 -0700

    First unit test and url mapping, dummy view

--- crashlacma/urls.py
+++ crashlacma/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'crashlacma.views.home', name='home'),
+    url(r'^$', 'main.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
