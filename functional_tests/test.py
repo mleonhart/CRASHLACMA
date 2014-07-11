e55e187c4a837a8ccc2e1f8b5a844575697074b4
carlynorama <carlynorama@gmail.com>
2014-07-11 16:26:25 -0700

    made things pretty. No tests or functionality.

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -16,8 +16,22 @@ class NewOutsideVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 
 		# She notices the page title and header mention CRASH LACMA
-		#but she does not yet know what that means
+		#but she doesn't really get what is going on.
 		self.assertIn('CRASH LACMA', self.browser.title)  
+		
+		# She notices sees and About link  
+		#in the main navigation area (check for style? location?)
+		nav_text = self.browser.find_element_by_id('id_new_item').text
+        self.assertIn('About', nav_text)
+		
+		# and decides to read what is on that page 
+		# by clicking on the link. 
+		
+		# she is on a page with about_us in the URL
+		# new_url = self.browser.current_url
+        #self.assertRegex(new_url, '/about_us')
+		
+		#Not really done yet
 		self.fail('Finish the test!')  
 
 if __name__ == '__main__':  
