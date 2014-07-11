e8542002fd35904cd350ececa6535e5af2033811
carlynorama <carlynorama@gmail.com>
2014-07-11 14:01:27 -0700

    updated test

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,8 +1,27 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewOutsideVisitorTest(unittest.TestCase): 
 
-assert 'Django' in browser.title
+def setUp(self):
+    self.browser = webdriver.Firefox()
+    self.browser.implicitly_wait(3)
 
-browser.quit()
+def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self): 
+        # Edith has heard about a cool project happening at LACMA and
+        # decided to find out more about it via its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention CRASH LACMA
+        #but she does not yet know what that means
+        self.assertIn('CRASH LACMA', self.browser.title)  
+        self.fail('Finish the test!')  
+
+        # She is invited to enter a to-do item straight away
+        [...rest of comments as before]
+
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore')  
