62743dc38abcd2aac804448505a66d6f1676366b
carlynorama <carlynorama@gmail.com>
2014-07-12 15:07:18 -0700

    victorious! unittest gets stdout from object handler

--- tweet_handler/management/commands/gettwitterfeed.py
+++ tweet_handler/management/commands/gettwitterfeed.py
@@ -8,11 +8,13 @@ from django.core.management.base import BaseCommand, CommandError
 class Command(BaseCommand):
 	help = 'Gets twiiter feed for hashtag #CRASHLACMA'
 
+	#python object function operates on Command
 	def handle(self, *args, **options): #there are no args or options yet. 
 		#use stdout because this will be run as cron job eventually
 		#(wait isn't print() in 3.x stdout?)
 		self.stdout.write('Hello There!')
 		
+	#python class function from Command		
 	def test_me(): #there are no args or options yet. 
 		#use stdout because this will be run as cron job eventually
 		#(wait isn't print() in 3.x stdout?)
--- tweet_handler/tests.py
+++ tweet_handler/tests.py
@@ -2,13 +2,23 @@ from io import StringIO
 from unittest import TestCase
 from unittest.mock import patch
 
+from django.core.management import call_command
 
 from tweet_handler.management.commands import gettwitterfeed
 
 #http://chimera.labs.oreilly.com/books/1230000000393/ch14.html#_solution_226
 #other options
 #http://www.obeythetestinggoat.com/how-to-log-exceptions-to-stderr-in-django.html
+#and
+#http://stackoverflow.com/questions/22571320/can-django-manage-py-custom-commands-return-a-value-how-or-why-not
 
+class TestCustomMangementScriptStdout(TestCase):
+	def test_message_gets_to_stdout(self):
+		expected_message = 'Hello There!\n'
+		actual_message = StringIO()
+		call_command('gettwitterfeed', stdout=actual_message)
+		self.assertEqual(actual_message.getvalue(), expected_message)
+		
 class TestMessagePrint(TestCase):
     def test_message_gets_to_stdout(self):
         expected_message = 'Hello There!'
