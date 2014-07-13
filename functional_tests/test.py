50c1d3ad21d9674b6c44a9b4314935e83657c4ef
carlynorama <carlynorama@gmail.com>
2014-07-12 22:35:30 -0700

    can now functional test the pretty

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import unittest
 
+
 class NewOutsideVisitorTest(unittest.TestCase): 
 
 	def setUp(self):
@@ -60,34 +61,42 @@ class ProjectorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 	
-	def check_link(self, link_location_id, link_text, expected_url):
-		nav_text = self.browser.find_element_by_id(link_location_id).text
-		self.assertIn(link_text, nav_text)
-		self.browser.find_element_by_link_text(link_text).click()
-		new_url = self.browser.current_url
-		self.assertRegex(new_url, expected_url)
-		
-	def check_page_content(self, has_text, doesnothave_text):
+	# not DRY, whats a better test for this page?
+	def check_projection_page_content(self, has_text, doesnothave_text):
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertIn(has_text, page_text)
 		self.assertNotIn(doesnothave_text, page_text)
-		
-	def test_can_navigate_to_projection_pages(self): 
-		# Edith has heard about a cool project happening at LACMA and
-		# decided to find out more about it via its homepage
-		self.browser.get('http://localhost:8000')
 
-		# She notices the page title and header mention CRASH LACMA
-		# but she doesn't really get what is going on.
-		self.assertIn('CRASH LACMA', self.browser.title)  
+	# Michele needs to start up the projection so she navigates to the
+	# server/projection page
+	def test_can_load_projection_page(self): 
+		#need refactoring to not reference localhost:8000
+		self.browser.get('http://localhost:8000/projection') 
 		
-		# and decides to clicking on the About link in the main navigation area
-		# and it takes her to a page with a comprehensible URL and relevant content. 
-		self.check_link('main-navbar', 'projection', '/projection')
-		self.check_page_content('LACMA', 'Donec sed odio dui.')
+	# she notices....
+	def test_layout_and_styling(self):
+		#not dry, hard url dependent
+		self.browser.get('http://localhost:8000/projection')
+
+		#should we test 800 by 600?
+		self.browser.set_window_size(1024, 768)
+
+		# She notices the content is nicely centered ...
+		#(not yet checking for vertical centeredness)
+		content_container = self.browser.find_element_by_class_name('content')
+		self.assertAlmostEqual(
+			content_container.location['x'] + content_container.size['width'] / 2,
+			512,
+			delta=5
+		)
+		
+		# ... and the background is a sleek black
+		body_element = self.browser.find_element_by_tag_name('body')
+		self.assertEqual(body_element.value_of_css_property('background-color'), 'rgba(0, 0, 0, 1)')
+		
 
 		#Not really done yet
-		self.fail('Finish the test!')  
+		#self.fail('Finish the test!')  
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')  
